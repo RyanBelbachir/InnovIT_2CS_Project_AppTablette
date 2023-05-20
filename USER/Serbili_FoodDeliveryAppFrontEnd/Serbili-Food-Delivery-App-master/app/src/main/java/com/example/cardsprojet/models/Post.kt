989532403f4531914e7359237d05490f97ebf2c7
Kamyl Taibi <93787662+Kamy1tb@users.noreply.github.com>
2023-05-20 18:46:22 +0100

    Delete USER directory

--- USER/Serbili_FoodDeliveryAppFrontEnd/Serbili-Food-Delivery-App-master/app/src/main/java/com/example/cardsprojet/models/Post.kt
+++
@@ -1,8 +0,0 @@
-package com.example.cardsprojet.models
-
-data class Post (
-    val userId: Int = 0,
-    val id: Int? = null,
-    val title: String? = null,
-    val body: String? = null
-)
--- USER/Serbili_FoodDeliveryAppFrontEnd/Serbili-Food-Delivery-App-master/app/src/main/java/com/example/cardsprojet/models/type_resto.kt
+++
@@ -1,8 +0,0 @@
-package com.example.cardsprojet.models
-
-data class type_resto(
-    val id_type: Int,
-    val nom: String,
-    val restaurant: String
-
-)
